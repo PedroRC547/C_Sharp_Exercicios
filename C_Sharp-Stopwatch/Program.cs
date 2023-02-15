f20819fb50cbe30b3f54458038666bbf3eeb1f29
PedroRC547 <candianipedro547@gmail.com>
2023-02-14 23:05:40 -0300

    Projetos C#

---
+++ C_Sharp-Stopwatch/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start(6);
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar: ");
+
+            String data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0,data.Length - 1));   
+            int multiplier = 1;
+
+            if(type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if(type == '0')
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
---
+++ C_Sharp-lerEmail/Main.cs
@@ -0,0 +1,9 @@
+﻿using ReadEmail;
+
+var email = new Email("outlook.office365.com", "<email>", "<senha do email>");
+
+await email.Connect();
+
+var message = email.GetMimeMessages();
+
+//Console.ReadLine();
