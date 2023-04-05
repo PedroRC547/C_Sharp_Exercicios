dbc7d493439cf362293fe0075f82390d298d2130
Pedro Candiani <pedro.candiani@sintese.local>
2023-04-05 18:55:12 -0300

    Exercicio de POO Balta

---
+++ C-Sharp-POO/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            var pessoaA = new Pessoa(1, "Pedro Rodrigues");
+            var pessoaB = new Pessoa(1, "Pedro Rodrigues");
+
+            Console.WriteLine(pessoaA.Equals(pessoaB)); //Falso
+
+        }
+    }
+
+    public class Pessoa : IEquatable<Pessoa>
+    {
+        public Pessoa(int id, string nome)
+        {
+            Id = id;
+            Nome= nome;
+        }
+
+        public int Id { get; set; }
+        public string Nome {get; set; }
+
+        public bool Equals(Pessoa? pessoa)
+        {
+            return Id == pessoa.Id;
+        }
+    }
+
+}
+
