78d19d94a619d16d88f2290b5a9c72146a907b18
Pedro Candiani <pedro.candiani@sintese.local>
2023-04-06 12:08:31 -0300

    Generics

---
+++ C_Sharp_POO_Generics/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var person = new Person();
+            var payment = new Payment();
+            var subscription = new Payment();
+            var context = new DataContext<IPerson, Payment, Subscription>();
+            context.Save(person);
+            context.Save(payment);
+            context.Save(subscription);
+        }
+
+    }
+
+    public class DataContext<P, PA, S> where P : IPerson where PA : Payment where S: Subscription
+    {
+        public void Save(P entity)
+        {
+
+        }
+        public void Save(PA entity)
+        {
+
+        }
+        public void Save(S entity)
+        {
+
+        }
+    }
+    
+    public interface IPerson
+    {
+
+    }
+
+    public class Person : IPerson{}
+
+    public class Payment{}
+
+    public class Subscription{}
+}
+
