26480ca9633dd98fff1b2f10ced312208fb3e931
Pedro Candiani <pedro.candiani@sintese.local>
2023-04-06 11:42:48 -0300

    Events

---
+++ C_Sharp_POO_Events/Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var room = new Room(3);
+
+            room.RoomSoldOutEvent += OnRoomSoldOut;
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+        }
+
+        static void OnRoomSoldOut(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sala lotada");
+        }
+    }
+
+    public class Room
+    {
+        public Room(int seats)
+        {
+            Seats = seats;
+            seatsInUse = 0;
+        }
+
+        private int seatsInUse = 0;
+        public int Seats {get; set;}
+
+        public void ReserveSeat()
+        {
+            seatsInUse++;
+            if(seatsInUse > Seats)
+            {
+                OnRoomSoldOut(EventArgs.Empty);
+            }else{
+                Console.WriteLine("Assento Reservado");
+            }
+        }
+
+        public event EventHandler RoomSoldOutEvent;
+
+        protected virtual void OnRoomSoldOut(EventArgs e)
+        {
+            EventHandler handler = RoomSoldOutEvent;
+            handler?.Invoke(this, e);
+        }
+
+    }
+}
+
