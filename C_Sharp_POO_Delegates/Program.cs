8a963ea964bff3b6ae5e542df755a35281bd9cc1
Pedro Candiani <pedro.candiani@sintese.local>
2023-04-06 10:11:00 -0300

    Delegates

---
+++ C_Sharp_POO_Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Payments
+{
+    class Program
+    {
+        static void RealizarPagamento(double valor){
+            Console.WriteLine($"Pago o valor de {valor}");
+        }
+        static void Main(string[] args)
+        {   
+            
+            var pagar = new Pagamento.Pagar(RealizarPagamento);
+            pagar(25);
+        }
+    }
+
+    public class Pagamento
+    {
+        public delegate void Pagar(double valor);
+
+    }
+
+}
+
